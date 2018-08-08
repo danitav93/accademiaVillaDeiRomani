14f8bbac136028c25cb4e8c71f0f1ab2d76444ad
Daniele <danitav93@hotmail.it>
2018-08-08 13:19:59 +0200

    Update AccademiaVillaDeiRomaniApplication.java

--- accademiaVillaDeiRomani/src/main/java/com/nodelab/accademiaVillaDeiRomani/AccademiaVillaDeiRomaniApplication.java
+++ accademiaVillaDeiRomani/src/main/java/com/nodelab/accademiaVillaDeiRomani/AccademiaVillaDeiRomaniApplication.java
@@ -14,7 +14,7 @@ public class AccademiaVillaDeiRomaniApplication  {
 		SpringApplication.run(AccademiaVillaDeiRomaniApplication.class, args);
 		
 		
-		vcbvcbcvbcvbcv
+		vcbvcbcvbcvbcv pino
 		
 		
 	}
