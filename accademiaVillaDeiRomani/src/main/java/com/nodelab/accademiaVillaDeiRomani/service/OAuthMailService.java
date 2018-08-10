9113c9ae27a079bce5f66be87432b1d4c4e14a3e
Luca-miniucchi <Luca-miniucchi@users.noreply.github.com>
2018-08-10 20:42:53 +0200

    Commit Locale Luca 3

---
+++ accademiaVillaDeiRomani/src/main/java/com/nodelab/accademiaVillaDeiRomani/service/OAuthMailService.java
@@ -0,0 +1,11 @@
+package com.nodelab.accademiaVillaDeiRomani.service;
+
+/*bella*/
+
+public interface OAuthMailService {
+
+
+	
+	public String getMailAccessToken();
+	
+}
---
+++ accademiaVillaDeiRomani/src/main/java/com/nodelab/accademiaVillaDeiRomani/service/OAuthMailServiceImpl.java
@@ -0,0 +1,58 @@
+package com.nodelab.accademiaVillaDeiRomani.service;
+
+/*bella*/
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.nodelab.accademiaVillaDeiRomani.model.GoogleAccessToken;
+import com.nodelab.accademiaVillaDeiRomani.repository.GoogleAccessTokenRepository;
+
+@Service("oAuthMailServiceImpl")
+public class OAuthMailServiceImpl implements OAuthMailService{
+
+	@Autowired
+	GoogleAccessTokenRepository googleAccessTokenRepository;
+	
+	@Autowired
+	ApplicationInfoService applicationInfoService;
+	
+	@Override
+	public String getMailAccessToken() {
+	
+		List<GoogleAccessToken> googleAccessTokenList= googleAccessTokenRepository.findAll();
+		GoogleAccessToken googleAccessToken;
+		if (googleAccessTokenList==null || googleAccessTokenList.isEmpty()) {
+			googleAccessToken=new GoogleAccessToken();
+			googleAccessToken.setExpire(0L);
+		} else {
+			googleAccessToken=googleAccessTokenList.get(0);
+		}
+		
+		if (System.currentTimeMillis()>googleAccessToken.getExpire()) {
+			
+			
+			try {
+				GoogleTokenResponse response =
+				        new GoogleRefreshTokenRequest(new NetHttpTransport(), new JacksonFactory(), applicationInfoService.getGoogleRefreshToken(), applicationInfoService.getGoogleClientId(), applicationInfoService.getGoogleClientSecret()).execute();
+				googleAccessToken.setAccessToken(response.getAccessToken());
+				googleAccessToken.setExpire(System.currentTimeMillis()+(((Number)response.getExpiresInSeconds()).intValue()*1000));
+				
+				googleAccessToken=googleAccessTokenRepository.save(googleAccessToken);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+			
+		}
+		return googleAccessToken.getAccessToken();
+	}
+
+}
